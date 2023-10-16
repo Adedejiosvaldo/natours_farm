75f3682722a61252227d309a2d656826f112da40
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-16 11:21:45 +0100

    Working with URL parameters

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 const fs = require("fs");
+const { parse } = require("path");
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
@@ -39,6 +40,29 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const { id } = req.params;
+
+  //   console.log(id);
+  const tour = toursData.find((el) => el.id === parseInt(id));
+
+  if (!tour) {
+    return res.status(400).json({
+      staus: "Error",
+      data: {
+        message: "ID does not exist",
+      },
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: tour,
+    },
+  });
 });
 
 app.listen(port, () => {
