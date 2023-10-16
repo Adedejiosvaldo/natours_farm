e1f4ad1e2bd23c64bb44ba8d1f3c84951115664f
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-16 10:56:05 +0100

    POST Request

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const port = process.env.PORT || 5000;
+app.use(express.json());
 
 const toursData = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -15,6 +17,28 @@ app.get("/api/v1/tours", (req, res) => {
       toursData,
     },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = toursData[toursData.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  //Push
+  toursData.push(newTour);
+
+  //Write updated File
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursData),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
