7e92f0a4ecc7ce53d4bccd08406af71c1d25ee90
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-17 14:28:05 +0100

    Feature - Added 3rd Party Middleware

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express");
+const fs = require("fs");
+const morgan = require("morgan");
 
 const app = express();
-const fs = require("fs");
-const { parse } = require("path");
 const TourRouter = require("./routes/Tours");
 const port = process.env.PORT || 5000;
-app.use(express.json());
 
+//Middleware
+app.use(express.json());
 app.use((req, res, next) => {
   console.log("Hello from MiddleWare");
   next();
 });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+app.use(morgan("dev"));
+
+//Routes
 app.use("/api/v1/tours", TourRouter);
 
 app.listen(port, () => {
