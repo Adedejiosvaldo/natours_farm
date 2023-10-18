3bafc8ef4724cf1b4836dae93f27885aa031f4b3
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-18 09:42:13 +0100

    Feature- Env Variables

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const fs = require("fs");
 const morgan = require("morgan");
-
+dotenv.config({ path: "./config.env" });
 const app = express();
-app.use(morgan("dev"));
+
 const TourRoutes = require("./routes/Tours");
 const UserRoutes = require("./routes/Users");
 
+//Middlewares
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 //Routes
 app.use("/api/v1/tours", TourRoutes);
