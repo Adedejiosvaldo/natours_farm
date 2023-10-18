054752ea22c95644ad44a1222d64a317bae89c5b
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-18 10:13:13 +0100

    Feature- Eslint and Prettier Config

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const fs = require("fs");
-const morgan = require("morgan");
-dotenv.config({ path: "./config.env" });
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config({ path: './config.env' });
 const app = express();
 
-const TourRoutes = require("./routes/Tours");
-const UserRoutes = require("./routes/Users");
+const TourRoutes = require('./routes/Tours');
+const UserRoutes = require('./routes/Users');
 
 //Middlewares
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 //Routes
-app.use("/api/v1/tours", TourRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use('/api/v1/tours', TourRoutes);
+app.use('/api/v1/users', UserRoutes);
 
 module.exports = app;
