8e544cd60aafbb037adc4b5be7125c1d4a44b26f
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-24 05:45:03 +0100

    Feature - Updating and Deleting

--- controller/Tours.js
+++ controller/Tours.js
@@ -39,11 +39,38 @@ const getATour = async (req, res) => {
   }
 };
 
-const updateTour = async (req, res) => {};
+const updateTour = async (req, res) => {
+  try {
+    const tour = await Tours.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      //   runValidators: true,
+    });
+
+    res.status(200).json({ status: 'Success', data: { tour } });
+  } catch (error) {
+    res.status(400).json({ status: 'Failed', msg: error.message });
+  }
+};
+
+const deleteTour = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const tour = await Tours.findByIdAndDelete(id);
+
+    res.status(200).json({
+      status: 'Success',
+      msg: `Successfully deleted tour with ID ${id}`,
+    });
+  } catch (error) {
+    return res.status(404).json({ status: 'Sucess' });
+  }
+};
 
 module.exports = {
   getATour,
   getAllTours,
   createTour,
   updateTour,
+  deleteTour,
 };
--- routes/Tours.js
+++ routes/Tours.js
@@ -4,10 +4,11 @@ const {
   createTour,
   getATour,
   updateTour,
+  deleteTour,
 } = require('../controller/Tours');
 const router = express.Router();
 
 router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getATour).patch(updateTour).delete();
+router.route('/:id').get(getATour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
