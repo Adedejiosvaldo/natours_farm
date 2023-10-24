507ed95f883270b2ff07647394ab3614bae26363
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-24 07:04:26 +0100

    Feature - Updated Tours Model

--- model/Tours.js
+++ model/Tours.js
@@ -5,8 +5,29 @@ const TourSchema = mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, 'A toour must have difficultly level'],
+  },
+
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+
+  ratingQuantity: {
     type: Number,
     default: 4.5,
   },
@@ -15,6 +36,35 @@ const TourSchema = mongoose.Schema({
     type: Number,
     required: [true, 'Price must be added'],
   },
+
+  priceDiscount: Number,
+
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a Description'],
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have civer image'],
+  },
+
+  //Array of images - Array of strings
+  images: [String],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  //Array of date - Difrent dates of tours
+  startDates: [Date],
 });
 
 module.exports = mongoose.model('Tours', TourSchema);
