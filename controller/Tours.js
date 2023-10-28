828bca906aa256c6cd0559e37071b25c07c1ec64
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-10-28 07:28:06 +0100

    Feature - Added Pagination

--- controller/Tours.js
+++ controller/Tours.js
@@ -32,12 +32,27 @@ const getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
-    //Field Limiting
+    //C- Field Limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    // D- Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    // To check that the pagination does not exist the available doc
+    if (req.query.page) {
+      const numberOfTours = await Tours.countDocuments();
+      if (skip >= numberOfTours) {
+        throw new Error('This page does not exist');
+      }
     }
     //Executing the query
     const allTours = await query;
