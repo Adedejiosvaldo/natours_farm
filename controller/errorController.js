70137774d2bf6cdf73095e4b3a7a34d77b0e1b4c
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-11-09 13:55:49 +0100

    ErrorHandling - Errors during dev vs prod

--- controller/errorController.js
+++ controller/errorController.js
@@ -1,9 +1,40 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'fail';
-
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational Errors - Trusted error - send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  //   Unknown error : Do not leak errors
+  else {
+    // 1) Log errors
+    console.error('Error', err);
+    // 2) Send generic errors
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail';
+  //We want to specify the error messages to get
+  // when working in the two env
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
