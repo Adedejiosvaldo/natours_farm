ac02a6077ad99711d7fac0794a65c098cbf6a821
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-11-10 13:09:09 +0100

    ErrorHandling - Handling uncaught rejection

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const dotenv = require('dotenv');
 const app = require('./app');
 const connectDB = require('./db/connect');
 
+process.on('uncaughtException', (err) => {
+  console.log('Shutting down');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 dotenv.config({ path: './config.env' });
+
 const DB = process.env.DATABASE_URL.replace('<Password>', process.env.PASSWORD);
 
 connectDB(DB);
@@ -11,9 +20,10 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Running on ${port}`);
 });
+
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log(err.message, 'Shutting down');
+  console.log(err);
+  console.log('Shutting down');
   server.close(() => {
     process.exit(1);
   });
