77efc2c650ea8b50ff5edb851db5162dbd62c026
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-11-11 16:07:19 +0100

    Authentification: Create user model

---
+++ model/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Incorrect email : Enter Correct EMail'],
+  },
+  photoUrl: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide email'],
+    minlength: [8, 'Minimum length of 8'],
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please provide email'],
+    minlength: [8, 'Minimum length of 8'],
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
