b0295d9408212d4c110d575de2827ddf56e7d740
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-11-13 21:51:04 +0100

    Authentification: Password management

--- model/User.js
+++ model/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
@@ -29,12 +30,23 @@ const UserSchema = mongoose.Schema({
     required: [true, 'Kindly Confirm Password'],
     minlength: [8, 'Minimum length of 8'],
     validate: {
+      //Works only on .create and .save
       validator: function (el) {
         return el === this.password;
       },
-      message: 'Password do not match - ({VALUE})',
+      message: 'Password do not match',
     },
   },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  //hash password with cpst of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Remove the confirm password
+  this.confirmPassword = undefined;
 });
 
 module.exports = mongoose.model('User', UserSchema);
