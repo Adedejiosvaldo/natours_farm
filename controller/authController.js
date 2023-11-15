ab77038ff68f65b8b777df82987a7b182185e6f9
adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-11-15 10:05:16 +0100

    Authentification: Corrected signup bug

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,12 @@
 const User = require('../model/User');
 const catchAsyncErrors = require('../utils/catchAsync');
 const signUp = catchAsyncErrors(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  });
 
   res.status(201).json({
     status: 'sucess',
