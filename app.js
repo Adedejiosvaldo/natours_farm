bf9442b129f624334ad327a86d1590bd2f1aabab
Joseph Adedeji <adedejiosvaldo@gmail.com>
2023-12-28 00:29:50 +0100

    Security - Setting Security HTTP headers (Helmet)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
+const helmet = require('helmet');
 dotenv.config({ path: './config.env' });
 const app = express();
 
@@ -10,19 +10,26 @@ const UserRoutes = require('./routes/Users');
 const AppError = require('./utils/appError');
 const errorController = require('./controller/errorController');
 const rateLimiter = require('express-rate-limit');
+
 //Middlewares
+//  Set Security HTTP Headers
+app.use(helmet());
+// Development logginf
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// Limit request from same IP
 const limiter = rateLimiter({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many request from this IP, Please Try again in an hour',
 });
-
 app.use('/api', limiter);
-app.use(express.json());
+
+// Body Parser - Reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving Static Files
 app.use(express.static(`${__dirname}/public`));
 
 //Routes
