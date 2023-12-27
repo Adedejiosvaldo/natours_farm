f640954f6b6ead587cf1b54c4b202cc7bf784c66
Joseph Adedeji <adedejiosvaldo@gmail.com>
2023-12-28 00:23:38 +0100

    Security - API Limiting

--- app.js
+++ app.js
@@ -9,12 +9,19 @@ const TourRoutes = require('./routes/Tours');
 const UserRoutes = require('./routes/Users');
 const AppError = require('./utils/appError');
 const errorController = require('./controller/errorController');
-
+const rateLimiter = require('express-rate-limit');
 //Middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimiter({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, Please Try again in an hour',
+});
+
+app.use('/api', limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
